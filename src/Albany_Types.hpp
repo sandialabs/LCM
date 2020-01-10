9efe4c3e57fa8bde87833826817c7726f03424d8
Alejandro Mota <amota@sandia.gov>
2020-01-10 15:42:17 -0800

    Fix clang-format

---
+++ src/Albany_Types.hpp
@@ -0,0 +1,10 @@
+//*****************************************************************//
+//    Albany 3.0:  Copyright 2016 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+#if !defined(Albany_Types)
+#include "Albany_SacadoTypes.hpp"
+#include "PHAL_AlbanyTraits.hpp"
+#include "PHAL_Dimension.hpp"
+#endif // Albany_Types

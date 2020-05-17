50101e64159c8c0bdbe80f019c08664c123a699d
Irina Tezaur <ikalash@sandia.gov>
2020-05-16 19:30:36 -0700

    ACE: adding commented-out code from ACEpermafrost_Def.hpp, to go through and make work within the ACEThermalParameters evaluator next week.

--- src/LCM/evaluators/ACEThermalParameters_Def.hpp
+++ src/LCM/evaluators/ACEThermalParameters_Def.hpp
@@ -106,6 +106,7 @@ void
 ACEThermalParameters<EvalT, Traits>::evaluateFields(
     typename Traits::EvalData workset)
 {
+  double current_time = workset.current_time; 
   std::string eb_name = workset.EBName; 
   Teuchos::ParameterList& sublist 
 	  = material_db_->getElementBlockSublist(eb_name, "ACE Thermal Parameters"); 
@@ -124,6 +125,226 @@ ACEThermalParameters<EvalT, Traits>::evaluateFields(
         sal_eb = interpolateVectors(z_above_mean_sea_level_eb, salinity_eb, height);
       }
       bluff_salinity_(cell, qp) = sal_eb;
+      auto const time_eb = this->queryElementBlockParameterMap(eb_name, time_map_);
+      auto const sea_level_eb = this->queryElementBlockParameterMap(eb_name, sea_level_map_); 
+      const ScalarT sea_level = sea_level_eb.size() > 0 ?  
+          interpolateVectors(time_eb, sea_level_eb, current_time) :
+          -999.0;
+      // Thermal calculation
+      // Calculate the depth-dependent porosity
+      // NOTE: The porosity does not change in time so this calculation only needs
+      //       to be done once, at the beginning of the simulation.
+      auto porosity_eb = this->queryElementBlockParameterMap(eb_name, porosity0_map_);
+      auto porosity_from_file_eb = this->queryElementBlockParameterMap(eb_name, porosity_from_file_map_); 
+      if (porosity_from_file_eb.size() > 0) {
+        porosity_eb = interpolateVectors(
+          z_above_mean_sea_level_eb, porosity_from_file_eb, height);
+      }
+      porosity_(cell, qp) = porosity_eb;
+      //IKT, FIXME: check with Jenn regarding the following block of code
+      //We don't have is_erodible anymore...  what should logic be? How should 
+      //factor be defined? 
+      // Calculate the salinity of the grid cell
+      /*if ((is_erodible == true) && (height <= sea_level)) {
+        RealType const cell_half_width    = 0.5 * element_size;
+        RealType const cell_exposed_area  = element_size * element_size;
+        RealType const cell_volume        = cell_exposed_area * element_size;
+        RealType const per_exposed_length = 1.0 / element_size;
+        RealType const factor             = per_exposed_length * salt_enhanced_D_;
+        ScalarT const  zero_sal(0.0);
+        ScalarT const  sal_curr  = bluff_salinity_(cell, pt);
+        ScalarT        ocean_sal = salinity_base_;
+        if (ocean_salinity_.size() > 0) {
+          ocean_sal = interpolateVectors(time_, ocean_salinity_, current_time);
+        }
+        ScalarT const sal_diff   = ocean_sal - sal_curr;
+        ScalarT const sal_grad   = sal_diff / cell_half_width;
+        ScalarT const sal_update = sal_grad * delta_time * factor;
+        ScalarT       sal_trial  = sal_curr + sal_update;
+        if (sal_trial < zero_sal) sal_trial = zero_sal;
+        if (sal_trial > ocean_sal) sal_trial = ocean_sal;
+        bluff_salinity_(cell, qp) = sal_trial;
+      }
+      ScalarT const sal = bluff_salinity_(cell, qp);*/
+
+      // Calculate melting temperature
+      /*ScalarT sal15(0.0);
+      if (sal > 0.0) { sal15 = std::sqrt(sal * sal * sal); }
+      auto const pressure_fixed = 1.0;
+      // Tmelt is in Kelvin
+      ScalarT const Tmelt = -0.057 * sal + 0.00170523 * sal15 -
+                          0.0002154996 * sal * sal -
+                          0.000753 / 10000.0 * pressure_fixed + 273.15;
+       */
+      //IKT, FIXME: the following does not make sense in the context of this 
+      //evaluator I think...  what does tdot_ mean here?  Anyway, it looks like 
+      //tdot_ is not used in ACEpermafrost_Def.hpp, so we don't need to define tdot_.
+      // Calculate temperature change
+      /*auto const dTemp = Tcurr - Told;
+      if (delta_time > 0.0) {
+        tdot_(cell, pt) = dTemp / delta_time;
+      } else {
+        tdot_(cell, pt) = 0.0;
+      }*/
+      // Calculate the freezing curve function df/dTemp
+      // W term sets the width of the freezing curve.
+      // Smaller W means steeper curve.
+      // f(T) = 1 / (1 + e^(-W*(T-T0)))
+      // New curve, formulated by Siddharth, which shifts the
+      // freezing point to left or right:
+      // f(T) = 1 / (1 + e^(-(8/W)((T-T0) + (b*W))))
+      // W = true width of freezing curve (in Celsius)
+      // b = shift to left or right (+ is left, - is right)
+
+      /*ScalarT W = 10.0;  // constant value
+      // if (freezing_curve_width_.size() > 0) {
+      //  W = interpolateVectors(
+      //      z_above_mean_sea_level_, freezing_curve_width_, height);
+      //}
+
+      ScalarT const Tdiff = Tcurr - Tmelt;
+      ScalarT const arg   = -(8.0 / W) * (Tdiff + (f_shift_ * W));
+      ScalarT       icurr{1.0};
+      ScalarT       dfdT{0.0};
+      auto const    tol = 709.0;
+
+      // Update freeze curve slope and ice saturation
+      if (arg < -tol) {
+        dfdT  = 0.0;
+        icurr = 0.0;
+      } 
+      else if (arg > tol) {
+        dfdT  = 0.0;
+        icurr = 1.0;
+      } 
+      else {
+        auto const    eps = minitensor::machine_epsilon<RealType>();
+        ScalarT const et  = std::exp(arg);
+        if (et < eps) {  // etp1 ~ 1.0
+          dfdT  = -(W / 8.0) * et;
+          icurr = 0.0;
+        } 
+        else if (1.0 / et < eps) {  // etp1 ~ et
+          dfdT  = -(W / 8.0) / et;
+          icurr = 1.0 - 1.0 / et;
+        } 
+        else {
+          ScalarT const etp1 = et + 1.0;
+          dfdT               = -(W / 8.0) * et / etp1 / etp1;
+          icurr              = 1.0 - 1.0 / etp1;
+        }
+      }
+
+      bool sediment_given = false;
+      if ((sand_from_file_.size() > 0) && (clay_from_file_.size() > 0) &&
+         (silt_from_file_.size() > 0) && (peat_from_file_.size() > 0)) {
+        sediment_given = true;
+      }*/
+       /*
+      // BEGIN NEW CURVE //
+      ScalarT const Tdiff = Tcurr - Tmelt;
+
+      RealType const A = 0.0;
+      RealType const G = 1.0;
+      RealType const C = 1.0;
+      RealType const Q = 0.001;
+      RealType const B = 10.0;
+      RealType       v = 25.0;
+
+      if (sediment_given = true) {
+        auto sand_frac =
+            interpolateVectors(z_above_mean_sea_level_, sand_from_file_, height);
+        auto clay_frac =
+            interpolateVectors(z_above_mean_sea_level_, clay_from_file_, height);
+        auto silt_frac =
+            interpolateVectors(z_above_mean_sea_level_, silt_from_file_, height);
+        auto peat_frac =
+            interpolateVectors(z_above_mean_sea_level_, peat_from_file_, height);
+        v = (peat_frac * 5.0) + (sand_frac * 5.0) + (silt_frac * 25.0) +
+            (clay_frac * 70.0);
+      }
+      ScalarT const qebt = Q * std::exp(-B * Tdiff);
+
+      ScalarT icurr = A + ((G - A) / (pow(C + qebt, 1.0/v)));
+      ScalarT dfdT = ((B * Q * (G - A)) * pow(C + qebt, -1.0/v) + (qebt / Q)) / (v *
+                      (C + qebt));
+      // END NEW CURVE //
+     */
+      // Update the water saturation
+      /*ScalarT wcurr = 1.0 - icurr;
+
+      ScalarT calc_soil_heat_capacity;
+      ScalarT calc_soil_thermal_cond;
+      ScalarT calc_soil_density;
+      if (sediment_given == true) {
+        auto sand_frac =
+            interpolateVectors(z_above_mean_sea_level_, sand_from_file_, height);
+        auto clay_frac =
+            interpolateVectors(z_above_mean_sea_level_, clay_from_file_, height);
+        auto silt_frac =
+            interpolateVectors(z_above_mean_sea_level_, silt_from_file_, height);
+        auto peat_frac =
+            interpolateVectors(z_above_mean_sea_level_, peat_from_file_, height);
+
+       	// THERMAL PROPERTIES OF ROCKS, E.C. Robertson, U.S. Geological Survey
+        // Open-File Report 88-441 (1988).
+        // AGU presentation (2019) --> peat K value
+        // Gnatowski, Tomasz (2016) Thermal properties of degraded lowland
+        // peat-moorsh soils, EGU General Assembly 2016, held 17-22 April, 2016 in
+        // Vienna Austria, id. EPSC2016-8105 --> peat Cp value Cp values in [J/kg/K]
+        calc_soil_heat_capacity = (0.7e3 * sand_frac) + (0.6e3 * clay_frac) +
+                                  (0.7e3 * silt_frac) + (1.9e3 * peat_frac);
+        // K values in [W/K/m]
+        calc_soil_thermal_cond = (8.0 * sand_frac) + (0.4 * clay_frac) +
+                                 (4.9 * silt_frac) + (0.08 * peat_frac);
+        // Rho values in [kg/m3]
+        // Peat density from Emily Bristol
+        calc_soil_density =
+            ((1.0 - peat_frac) * soil_density_) + (peat_frac * 250.0);
+      }
+      // Update the effective material density
+      if (sediment_given == true) {
+        density_(cell, pt) =
+            (porosity * ((ice_density_ * icurr) + (water_density_ * wcurr))) +
+            ((1.0 - porosity) * calc_soil_density);
+      } 
+      else {
+        density_(cell, pt) =
+            (porosity * ((ice_density_ * icurr) + (water_density_ * wcurr))) +
+            ((1.0 - porosity) * soil_density_);
+      }
+
+      // Update the effective material heat capacity
+      if (sediment_given == true) {
+        heat_capacity_(cell, pt) = (porosity * ((ice_heat_capacity_ * icurr) +
+                                               (water_heat_capacity_ * wcurr))) +
+                                   ((1.0 - porosity) * calc_soil_heat_capacity);
+      } 
+      else {
+        heat_capacity_(cell, pt) = (porosity * ((ice_heat_capacity_ * icurr) +
+                                               (water_heat_capacity_ * wcurr))) +
+                                   ((1.0 - porosity) * soil_heat_capacity_);
+      }
+
+      // Update the effective material thermal conductivity
+      if (sediment_given == true) {
+        thermal_cond_(cell, pt) = pow(ice_thermal_cond_, (icurr * porosity)) *
+                                  pow(water_thermal_cond_, (wcurr * porosity)) *
+                                  pow(calc_soil_thermal_cond, (1.0 - porosity));
+      } 
+      else {
+        thermal_cond_(cell, pt) = pow(ice_thermal_cond_, (icurr * porosity)) *
+                                  pow(water_thermal_cond_, (wcurr * porosity)) *
+                                  pow(soil_thermal_cond_, (1.0 - porosity));
+      }
+      // Update the material thermal inertia term
+      thermal_inertia_(cell, pt) = (density_(cell, pt) * heat_capacity_(cell, pt)) -
+                                   (ice_density_ * latent_heat_ * dfdT);
+ 
+      // Return values
+      ice_saturation_(cell, pt)   = icurr;
+      water_saturation_(cell, pt) = wcurr;*/
+
       thermal_conductivity_(cell, qp) = thermal_conduct_eb; 
       thermal_inertia_(cell, qp) = thermal_inertia_eb; 
     }

646b5ecff5e10a701b557f04c64c5928b9f7c135
Irina Tezaur <ikalash@sandia.gov>
2020-06-05 15:03:00 -0700

    ACE: writing tests for sequential thermo-mechanical solver that compares thermal solution to the solution computed in the CuboidThermoMechanical3D_ThermalOnly test using Exodiff.
    Note that currently the sequential thermo-mechanical solver solves the mechanics and thermal
    problems separately without any coupling.  The coupling needs to be added as a next step.
    
    Note also that the mechanics part does nothing effectively - it computes a solution of all 0s.
    That needs to be switched over to the J2 Erosion material model, tested, and modified to
    solve a nontrivial problem.

---
+++ tests/LCM/ACE/CuboidSequentialThermoMechanical3D/runtest.cmake
@@ -0,0 +1,70 @@
+# 1. Run the program and generate the exodus output
+
+message("Running the command:")
+message("${TEST_PROG} " " ${TEST_ARGS}")
+
+EXECUTE_PROCESS(COMMAND ${TEST_PROG} ${TEST_ARGS}
+                RESULT_VARIABLE HAD_ERROR)
+
+if(HAD_ERROR)
+	message(FATAL_ERROR "Albany didn't run: test failed")
+endif()
+
+
+# 2. Find and run exodiff
+
+if (NOT SEACAS_EXODIFF)
+  message(FATAL_ERROR "Cannot find exodiff")
+endif()
+
+if(DEFINED MPIMNP AND ${MPIMNP} GREATER 1)
+  SET(EXODIFF_TEST0 ${SEACAS_EXODIFF} -i -m -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME}.4.0 ${REF_FILENAME}.4.0)
+  SET(EXODIFF_TEST1 ${SEACAS_EXODIFF} -i -m -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME}.4.1 ${REF_FILENAME}.4.1)
+  SET(EXODIFF_TEST2 ${SEACAS_EXODIFF} -i -m -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME}.4.2 ${REF_FILENAME}.4.2)
+  SET(EXODIFF_TEST3 ${SEACAS_EXODIFF} -i -m -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME}.4.3 ${REF_FILENAME}.4.3)
+ELSE()
+  SET(EXODIFF_TEST0 ${SEACAS_EXODIFF} -i -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME}.4.0 ${REF_FILENAME}.4.0)
+  SET(EXODIFF_TEST1 ${SEACAS_EXODIFF} -i -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME}.4.1 ${REF_FILENAME}.4.1)
+  SET(EXODIFF_TEST2 ${SEACAS_EXODIFF} -i -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME}.4.2 ${REF_FILENAME}.4.2)
+  SET(EXODIFF_TEST3 ${SEACAS_EXODIFF} -i -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME}.4.3 ${REF_FILENAME}.4.3)
+ENDIF()
+#if(DEFINED MPIMNP AND ${MPIMNP} GREATER 1)
+#  SET(EXODIFF_TEST ${SEACAS_EXODIFF} -i -m -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME} ${REF_FILENAME})
+#ELSE()
+#  SET(EXODIFF_TEST ${SEACAS_EXODIFF} -i -f ${TEST_NAME}.exodiff ${OUTPUT_FILENAME} ${REF_FILENAME})
+#ENDIF()
+
+message("Running the command:")
+message("${EXODIFF_TEST}")
+
+EXECUTE_PROCESS(
+    COMMAND ${EXODIFF_TEST0}
+    #OUTPUT_FILE ${TEST_NAME}.exodiff.out
+    RESULT_VARIABLE HAD_ERROR)
+if(HAD_ERROR)
+	message(FATAL_ERROR "Test failed")
+endif()
+
+EXECUTE_PROCESS(
+    COMMAND ${EXODIFF_TEST1}
+    #OUTPUT_FILE ${TEST_NAME}.exodiff.out
+    RESULT_VARIABLE HAD_ERROR)
+if(HAD_ERROR)
+	message(FATAL_ERROR "Test failed")
+endif()
+
+EXECUTE_PROCESS(
+    COMMAND ${EXODIFF_TEST2}
+    #OUTPUT_FILE ${TEST_NAME}.exodiff.out
+    RESULT_VARIABLE HAD_ERROR)
+if(HAD_ERROR)
+	message(FATAL_ERROR "Test failed")
+endif()
+
+EXECUTE_PROCESS(
+    COMMAND ${EXODIFF_TEST3}
+    #OUTPUT_FILE ${TEST_NAME}.exodiff.out
+    RESULT_VARIABLE HAD_ERROR)
+if(HAD_ERROR)
+	message(FATAL_ERROR "Test failed")
+endif()

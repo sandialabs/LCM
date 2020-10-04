397a75b904c0091c14f8995fd0bba7f5a545842b
Irina Tezaur <ikalash@sandia.gov>
2020-10-04 14:03:58 -0700

    Creating separate email post-pcoessing script for LCM from Albany, so that failures in LCM do not pollute tracking of ALbany tests.

---
+++ doc/LCMNightlyTestingEmail/process_nightlies_lcm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf TriBITS
+rm -rf test_history
+rm -rf *html
+rm -rf *out
+rm -rf *json
+rm -rf  lcmNightlyBuildsTwoif.csv 
+
+git clone git@github.com:TriBITSPub/TriBITS.git
+
+
+now=$(date +"%Y-%m-%d")
+
+./lcm_cdash_status.sh --date=$now --email-from-address=ikalash@skybrige-login5.sandia.gov --send-email-to=ikalash@sandia.gov,amota@sandia.gov,jmfrede@sandia.gov 
+
+

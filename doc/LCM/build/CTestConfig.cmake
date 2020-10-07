fe2961797cfd8b43e35dcdb1be0a3f0dc70a67da
Irina Tezaur <ikalash@sandia.gov>
2020-10-07 10:33:50 -0700

    Modifying overlooked line to post to LCM-specific CDash site.

--- doc/LCM/build/CTestConfig.cmake
+++ doc/LCM/build/CTestConfig.cmake
@@ -4,6 +4,6 @@ set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "sems-cdash-son.sandia.gov")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=Albany")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=Albany_LCM")
 set(CTEST_DROP_SITE_CDASH TRUE)
 

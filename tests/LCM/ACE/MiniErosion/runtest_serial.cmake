1f11d4aafc4d6283036ebff1a0bef4345779053d
Irina Tezaur <ikalash@sandia.gov>
2020-10-13 21:58:28 -0700

    Extending Erosion test to check mechanical output, not just thermal.

--- tests/LCM/ACE/MiniErosion/runtest_serial.cmake
+++ tests/LCM/ACE/MiniErosion/runtest_serial.cmake
@@ -19,8 +19,10 @@ endif()
 
 if(DEFINED MPIMNP AND ${MPIMNP} GREATER 1)
   SET(EXODIFF_TEST0 ${SEACAS_EXODIFF} -i -m -f ${TEST_NAME1}.exodiff -steps -1 ${OUTPUT_FILENAME1} ${REF_FILENAME1})
+  SET(EXODIFF_TEST1 ${SEACAS_EXODIFF} -i -m -f ${TEST_NAME2}.exodiff -steps -1 ${OUTPUT_FILENAME2} ${REF_FILENAME2})
 ELSE()
   SET(EXODIFF_TEST0 ${SEACAS_EXODIFF} -i -f ${TEST_NAME1}.exodiff -steps -1 ${OUTPUT_FILENAME1} ${REF_FILENAME1})
+  SET(EXODIFF_TEST1 ${SEACAS_EXODIFF} -i -f ${TEST_NAME2}.exodiff -steps -1 ${OUTPUT_FILENAME2} ${REF_FILENAME2})
 ENDIF()
 
 message("Running the command:")
@@ -28,6 +30,17 @@ message("${EXODIFF_TEST0}")
 
 EXECUTE_PROCESS(
     COMMAND ${EXODIFF_TEST0}
+    #OUTPUT_FILE ${TEST_NAME}.exodiff.out
+    RESULT_VARIABLE HAD_ERROR)
+if(HAD_ERROR)
+	message(FATAL_ERROR "Test failed")
+endif()
+
+message("Running the command:")
+message("${EXODIFF_TEST1}")
+
+EXECUTE_PROCESS(
+    COMMAND ${EXODIFF_TEST1}
     #OUTPUT_FILE ${TEST_NAME}.exodiff.out
     RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)

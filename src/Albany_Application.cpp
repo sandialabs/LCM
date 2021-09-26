c3369a464c2792594610b8e9fd51441553cca18b
Irina Tezaur <ikalash@sandia.gov>
2021-09-26 11:17:11 -0700

    Adding throw if trying to run LCM with transient adjoint sens, as I did not hook that up.

--- src/Albany_Application.cpp
+++ src/Albany_Application.cpp
@@ -638,6 +638,7 @@ Application::finalSetUp(
     solveParams.get(sensitivityToken, *oldSensitivityFlag);
   }
 
+
   // MPerego: Preforming post registration setup here to make sure that the
   // discretization is already created, so that  derivative dimensions are
   // known.
@@ -1659,6 +1660,16 @@ Application::determinePiroSolver(const Teuchos::RCP<Teuchos::ParameterList>& top
       piroSolverToken = "Unsupported";
     }
     piroParams->set("Solver Type", piroSolverToken);
+    
+    const bool compute_sens = problemParams->get<bool>("Compute Sensitivities", false);
+    std::string sens_method = "None"; 
+    if (compute_sens == true) {
+      sens_method = piroParams->get<std::string>("Sensitivity Method", "Forward"); 
+    }
+    if ((sens_method == "Adjoint") && (piroSolverToken == "Tempus")) {
+      ALBANY_ABORT(
+          "Albany/LCM does not have support for adjoint transient sensitivities!\n");
+    }
   }
 }
 

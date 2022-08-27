c858cb19ddea61fb9bb17912c4fc7b50fc978ca0
Alejandro Mota <amota@sandia.gov>
2022-08-26 18:07:58 -0700

    Add dynamic fits WIP

--- src/LCM/models/J2Erosion_Def.hpp
+++ src/LCM/models/J2Erosion_Def.hpp
@@ -263,6 +263,63 @@ class J2ErosionNLS : public minitensor::Function_Base<J2ErosionNLS<EvalT, M>, ty
   S const& Y_;
 };
 
+namespace {
+  RealType
+  E_fit_max(RealType const x, RealType const y)
+  {
+    return (210.0 - 528.0 * y - 209.0 * x + 936.0 * y * x) / 409.0;
+  }
+
+  RealType
+  Y_fit_max(RealType const x, RealType const y)
+  {
+    return (3.0 - 11.0 * y - 3.0 * x + 20.0 * y * x) / 9.0;
+  }
+
+  RealType
+  K_fit_min(RealType const x, RealType const y)
+  {
+    // return negative of fit 
+    return (-12.0 + 26.0 * y + 12.0 * x - 36.0 * y * x) / 10.0;
+  }
+
+  std::tuple<RealType, RealType, RealType>
+  unit_fit(RealType const ice_saturation, RealType const porosity)
+  {
+    auto const critical_porosity = 0.2;
+    auto const critical_ice_saturation = 0.4;
+    RealType E{1.0};
+    RealType Y{1.0};
+    RealType K{1.0};
+    auto const x = ice_saturation;
+    auto const y = porosity;
+    auto const xc = critical_porosity;
+    auto const yc = critical_ice_saturation;
+  if (x > xc && y > yc) {
+    Y = Y_fit_max(x, y);
+    E - E_fit_max(x, y);
+    K = K_fit_min(x, y);
+  }
+  else if (x <= xc) {
+    Y = Y_fit_max(xc, y) * x / xc;
+    E - E_fit_max(xc, y) * x / xc;
+    K = K_fit_min(xc, y) * x / xc;
+  }
+  else if (y <= yc) {
+    Y = Y_fit_max(x, yc) * y / yc;
+    E - E_fit_max(x, yc) * y / yc;
+    K = K_fit_min(x, yc) * y / yc;
+  }
+  else if ((x <= xc) && (y <= yc)) {
+    Y = Y_fit_max(xc, yc) * x * y / xc / yc;
+    E - E_fit_max(xc, yc) * x * y / xc / yc;
+    K = K_fit_min(xc, yc) * x * y / xc / yc;
+  }
+   return std::make_tuple(E, Y, K);
+  }
+
+} // anonymous namespace
+
 template <typename EvalT, typename Traits>
 KOKKOS_INLINE_FUNCTION void
 J2ErosionKernel<EvalT, Traits>::operator()(int cell, int pt) const
@@ -281,9 +338,6 @@ J2ErosionKernel<EvalT, Traits>::operator()(int cell, int pt) const
   auto const sea_level    = sea_level_.size() > 0 ? interpolateVectors(time_, sea_level_, current_time) : -999.0;
 
 #if defined(ICE_SATURATION)
-  RealType por_crit = 0.20;
-  RealType ice_crit = 0.40;
-  
   ScalarT const ice_saturation = ice_saturation_(cell, pt);
   auto const              peat = peat_from_file_.size() > 0 ?
                                  interpolateVectors(z_above_mean_sea_level_, peat_from_file_, height) :
@@ -297,53 +351,6 @@ J2ErosionKernel<EvalT, Traits>::operator()(int cell, int pt) const
   ScalarT          K = hardening_modulus_(cell, pt);
   ScalarT          Y = yield_strength_(cell, pt);
   
-  if (porosity > 0.999999999) {  // this means the material is an ice wedge
-    // do nothing. keep E, Y, and K from input deck.
-    E = E;
-    Y = Y;
-    K = K;
-  }
-  else if ((porosity > por_crit) && (ice_saturation > ice_crit)) {
-    Y = 3.0 - (11.0 * ice_saturation) - (3.0 * porosity) + (20.0 * ice_saturation * porosity);
-    E = 210.0 - (528.0 * ice_saturation) - (209.0 * porosity) + (936.0 * ice_saturation * porosity);
-    K = -12.0 + (26.0 * ice_saturation) + (12.0 * porosity) - (36.0 * ice_saturation * porosity);
-    Y = Y * 1.0e6;  // converts units to MPa
-    E = E * 1.0e6;
-    K = K * 1.0e6;
-  }
-  else if (porosity <= por_crit) {
-    Y = 3.0 - (11.0 * ice_saturation) - (3.0 * por_crit) + (20.0 * ice_saturation * por_crit) *
-        porosity / por_crit;
-    E = 210.0 - (528.0 * ice_saturation) - (209.0 * por_crit) + (936.0 * ice_saturation * por_crit) *
-        porosity / por_crit;
-    K = -12.0 + (26.0 * ice_saturation) + (12.0 * por_crit) - (36.0 * ice_saturation * por_crit) *
-        porosity / por_crit;
-    Y = Y * 1.0e6;  // converts units to MPa
-    E = E * 1.0e6;
-    K = K * 1.0e6;
-  }
-  else if (ice_saturation <= ice_crit) {
-    Y = 3.0 - (11.0 * ice_crit) - (3.0 * porosity) + (20.0 * ice_crit * porosity) *
-        ice_saturation / ice_crit;
-    E = 210.0 - (528.0 * ice_crit) - (209.0 * porosity) + (936.0 * ice_crit * porosity) *
-        ice_saturation / ice_crit;
-    K = -12.0 + (26.0 * ice_crit) + (12.0 * porosity) - (36.0 * ice_crit * porosity) *
-        ice_saturation / ice_crit;
-    Y = Y * 1.0e6;  // converts units to MPa
-    E = E * 1.0e6;
-    K = K * 1.0e6;
-  }
-  else if ((porosity <= por_crit) && (ice_saturation <= ice_crit)) {
-    Y = 3.0 - (11.0 * ice_crit) - (3.0 * por_crit) + (20.0 * ice_crit * por_crit) *
-        ice_saturation * porosity / (ice_crit * por_crit);
-    E = 210.0 - (528.0 * ice_crit) - (209.0 * por_crit) + (936.0 * ice_crit * por_crit) *
-        ice_saturation * porosity / (ice_crit * por_crit);
-    K = -12.0 + (26.0 * ice_crit) + (12.0 * por_crit) - (36.0 * ice_crit * por_crit) *
-        ice_saturation * porosity / (ice_crit * por_crit);
-    Y = Y * 1.0e6;  // converts units to MPa
-    E = E * 1.0e6;
-    K = K * 1.0e6;
-  }
   //Y = std::max(Y, (1.0e+03 + (peat * 1.6e4))); // residual yield strength
   Y = std::max(Y, soil_yield_strength_);
   //Y = std::max(Y, 1600.0); // absolute minimum residual yield strength

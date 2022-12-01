5519dda151f524627c62011794713749a8eda882
Jennifer M Frederick <jmfrede@sandia.gov>
2022-12-01 12:11:06 -0700

    Revert "Made new input deck variables for specifying Y, E, and SL weakening factors."
    This reverts commit d49b1537c4e2b4c0c4cd03f414975b140b28749b.

--- src/LCM/models/J2Erosion.hpp
+++ src/LCM/models/J2Erosion.hpp
@@ -93,12 +93,10 @@ struct J2ErosionKernel : public ParallelKernel<EvalT, Traits>
   RealType critical_angle_{0.0};
   RealType bulk_porosity_{0.0};
   RealType soil_yield_strength_{0.0};
-  RealType Y_weakening_factor_{1.0};
   RealType residual_elastic_modulus_{0.0};
-  RealType E_weakening_factor_{1.0};
+  RealType Y_weakening_factor_{1.0};
   RealType tensile_strength_{0.0};
   RealType strain_limit_{0.0};
-  RealType SL_weakening_factor_{0.0};
 
   // Params with depth or time:
   std::vector<RealType> z_above_mean_sea_level_;

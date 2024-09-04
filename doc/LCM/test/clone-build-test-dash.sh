542813a0e7971372b3fae8db42b6c11005efdba3
Alejandro Mota <amota@sandia.gov>
2024-09-04 10:34:44 -0700

    Add option to clone to specific SHA
    This is so that we can punt on the extensive changes
    required because of changes to STK

--- doc/LCM/test/clone-build-test-dash.sh
+++ doc/LCM/test/clone-build-test-dash.sh
@@ -11,16 +11,19 @@ for PACKAGE in $PACKAGES; do
 	    PACKAGE_NAME="Trilinos"
 	    REPO="git@github.com:trilinos/Trilinos.git"
             BRANCH="develop"
+            SHA="84124e66709dc3679e6dbef1f8352c9909ce7900"
 	    ;;
 	lcm)
 	    PACKAGE_NAME="LCM"
 	    REPO="git@github.com:sandialabs/LCM.git"
             BRANCH="main"
+            SHA="head"
 	    ;;
 	dtk)
 	    PACKAGE_NAME="DataTransferKit"
 	    REPO="git@github.com:ikalash/DataTransferKit"
             BRANCH="dtk-2.0-tpetra-static-graph"
+            SHA="head"
 	    ;;
 	*)
 	    echo "Unrecognized package option"
@@ -33,6 +36,9 @@ for PACKAGE in $PACKAGES; do
 	rm "$PACKAGE_DIR" -rf
     fi
     git clone -v -b "$BRANCH" "$REPO" "$PACKAGE_NAME" &> "$CHECKOUT_LOG"
+    if [[ "$SHA" != *"head"* ]]; then
+        git reset --hard "$SHA"
+    fi
 done
 
 # For now assume that if there is a DTK directory in the main LCM

bcdb75f16baffc029a5ee0cfe07a6dd9771f6e75
Irina Tezaur <ikalash@sandia.gov>
2025-08-12 20:35:32 -0700

    Correcting path in CTestConfig.cmake to push to new SRN CDash site.

--- doc/LCM/build/CTestConfig.cmake
+++ doc/LCM/build/CTestConfig.cmake
@@ -4,7 +4,7 @@ set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "albany-lcm-cdash.sandia.gov")
-set(CTEST_DROP_LOCATION "submit.php?project=Albany-LCM")
+set(CTEST_DROP_LOCATION "/submit.php?project=Albany-LCM")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 

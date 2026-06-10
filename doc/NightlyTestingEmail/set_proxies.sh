120ef245b18edade761c33f025f5ceb8644d810a
Irina K. Tezaur <ikalash@sandia.gov>
2026-06-10 17:33:22 -0600

    Checking in nightly test post-proc scripts, which currently run on cee-compute005.

---
+++ doc/NightlyTestingEmail/set_proxies.sh
@@ -0,0 +1,5 @@
+set | grep -i proxy
+ftp_proxy=http://user:pass@proxy.sandia.gov:80/
+http_proxy=http://user:pass@proxy.sandia.gov:80/
+https_proxy=http://user:pass@proxy.sandia.gov:80/
+no_proxy=localhost,localnets,.sandia.gov,127.0.0.1,169.254.0.0/16,forge.sandia.gov
